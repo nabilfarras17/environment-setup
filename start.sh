2bc9f297f1100344c92da5205f626fc53638da59
Nabil Farras <nabil.farras17@gmail.com>
2020-12-05 11:49:57 +0700

    start.sh

---
+++ start.sh
@@ -0,0 +1,52 @@
+# /bin/bash
+
+# Sudo
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+# Install sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# Install zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Install fonts-powerline
+sudo apt-get install fonts-powerline
+
+# Install ohMyZsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+source ~/.zshrc
+
+# Install nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+echo "export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm" >> .zshrc
+
+# Install spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# Install pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+CFLAGS="-I/opt/local/include/" \
+LDFLAGS="-L/opt/local/lib/" \
+pyenv install -v 2.7.13
+
+# Install sdkman
+curl -s "https://get.sdkman.io" | bash
+sdk install java 8.0.265-open
+sdk install scala 2.12.1
+
+# Install Telegram
+sudo add-apt-repository ppa:atareao/telegram
+sudo apt update && sudo apt install telegram
